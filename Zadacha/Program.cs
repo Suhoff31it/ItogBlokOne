ecbfd15cfb4c881fa4edf6a4e4233222fcd1cda8
Alexs <suhoff31it@gmail.com>
2023-04-30 22:34:49 +0300

    Написал код программы

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+string[] array = StringArray();
+string[] result = FindLess(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLess(string[] input, int n)
+{
+    string[] output = new string[CountLess(input, n)];
+
+    for (int i = 0, j = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CountLess(string[] input, int n)
+{
+    int count = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] StringArray()
+{
+    Write("Введите значения через пробел: ");
+    return ReadLine().Split(" ");
+}
